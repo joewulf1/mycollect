ecc22a0520404b1dc5ad75b077d0f01cafffa3a3
joewulf1 <joe.hiking@gmail.com>
2022-10-31 12:37:36 -0500

    database
    Some light database work, problem with calling database

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+bool loading = false;
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -27,7 +36,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
   // void _incrementCounter() {
   //   setState(() {
@@ -42,32 +51,59 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          children: [baseCard()],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("mycollections")
+              .snapshots(),
+          builder: (context, snapshot) {
+            print(snapshot.data?.docs.length);
+            if (!snapshot.hasData) {
+              return loading
+                  ? Loading()
+                  : const Center(
+                      child: Text(
+                      "Loading",
+                    ));
+            } else {
+              return ListView.builder(
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (context, index) =>
+                      baseCard(context, snapshot.data!.docs[index]));
+            }
+          }),
+      floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
-Widget baseCard() {
+Widget baseCard(BuildContext context, DocumentSnapshot document) {
   // This is the base of the homepage items //
   return Card(
-      margin: EdgeInsets.all(50),
       child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            debugPrint('Card tapped.');
-          },
           child: Container(
             height: 100,
             width: double.infinity,
-            child: Center(child: const Text("Test")),
+            child: Center(child: Text("Penis")),
           )));
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: SpinKitChasingDots(
+          color: Color(0xFF500000),
+          size: 50.0,
+        ),
+      ),
+    );
+  }
 }
